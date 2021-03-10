34f389ed8a2895a4a2e1e213097f0c7788dd76d3
mateusm <mateusm@vegait.com.br>
2021-03-10 13:27:47 -0300

    refact(project): adjust the project structure

---
+++ Models/PaginationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POC_paginacao.Models
+{
+    public class PaginationViewModel
+    {
+        public int Page { get; set; }
+
+        public int PageSize { get; set; }
+    }
+}
